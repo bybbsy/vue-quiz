b57fe794b30072e6393d3da5a033c58aab5f3ae0
Kirill P <jimmy_boi@mail.ru>
2022-05-06 13:20:42 +0300

    Added quizzes page

---
+++ src/views/QuizzesPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <main-layout>
+    <div class="quiz-list">
+      list quiz
+    </div>
+  </main-layout>
+</template>
+
+<script setup lang="ts">
+import mainLayout from '@/layouts/mainLayout.vue';
+
+</script>
